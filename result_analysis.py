5930a3e744036a27cb0f3668f2e354969bd89173
Jiangdz <jdz403798248@ruc.edu.cn>
2019-01-05 15:45:13 +0800

    写好所有图

--- result_analysis.py
+++ result_analysis.py
@@ -1,8 +1,13 @@
+#-------------------------------------------------
+# 结果分析
+#-------------------------------------------------
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 import time
+import os
 
 def read_model_result(path):
     timestamp = []
@@ -75,10 +80,124 @@ def read_resource(path):
                            'gpu_3':gpu_3,'gpu_4':gpu_4,'gpu_5':gpu_5,'gpu_6':gpu_6,'gpu_7':gpu_7})
     return cpu_memory,gpu
 
+def read_resource_1(path):
+    timestamp=[]
+    timestamp_gpu=[]
+    total_memo_perc=[]
+    p_memo_perc=[]
+    total_cpu_per=[]
+    p_cpu_per=[]
+    # pid=[]
+    gpu_0=[]
+    gpu_1=[]
+    gpu_2=[]
+    gpu_3=[]
+    gpu_4=[]
+    gpu_5=[]
+    gpu_6=[]
+    gpu_7=[]
+    with open(path) as f:
+        line = f.readline()
+        while line:
+            sp_line = line.split(' ')
+            if sp_line[1] == 'virtual':
+                # pid.append(int(sp_line[6]))
+                timestamp.append(time.mktime(time.strptime(sp_line[0], "%Y%m%d-%H:%M:%S")))
+                total_memo_perc.append(float(sp_line[4][:-2]))
+                p_memo_perc.append(float(sp_line[-1][:-2]))
+                line = f.readline()
+                sp_line = line.split(' ')
+                total_cpu_per.append(float(sp_line[3][:-2]))
+                p_cpu_per.append(float(sp_line[-1][:-2]))
+                line = f.readline()
+            elif sp_line[1] == 'GPU':
+                timestamp_gpu.append(time.mktime(time.strptime(sp_line[0], "%Y%m%d-%H:%M:%S")))
+                for gpu in [gpu_0, gpu_1, gpu_2, gpu_3, gpu_4, gpu_5, gpu_6, gpu_7]:
+                    sp_line = line.split(' ')
+                    gpu.append(float(sp_line[-1][:-1]))
+                    line = f.readline()
+            else:
+                print('Error!',line)
+                break
+
+    cpu_memory=pd.DataFrame({'timestamp':timestamp,'total_memo_perc':total_memo_perc,'p_memo_perc':p_memo_perc,
+                           'total_cpu_per':total_cpu_per,'p_cpu_per':p_cpu_per})
+
+    gpu=pd.DataFrame({'timestamp':timestamp_gpu,'gpu_0':gpu_0,'gpu_1':gpu_1,'gpu_2':gpu_2,
+                           'gpu_3':gpu_3,'gpu_4':gpu_4,'gpu_5':gpu_5,'gpu_6':gpu_6,'gpu_7':gpu_7})
+    return cpu_memory,gpu
+
+def tran_per(num):
+    num=round(num*100,2)
+    num=str(num)+'%'
+    return num
+
+def plot_xy(x,y,min_x,ax):
+    x=int(x)
+    ax.text(x=min_x, y=y, s=tran_per(y),fontsize=12)
+    ax.text(x=x, y=0, s=x,fontsize=12)
+    ax.plot([x, x], [-0.5, y], linestyle='--',color='grey')
+    ax.plot([min_x, x], [y, y], linestyle='--',color='grey')
+    ax.scatter([x, ], [y, ],color='grey')
+
 if __name__== '__main__':
-    zhfont1 = matplotlib.font_manager.FontProperties(family = 'Hiragino Sans GB',fname='/System/Library/Fonts/Hiragino Sans GB.ttc')
-    path='/Users/dongzhejiang/Downloads/log_save/cifar10365.log'
-    model_result=read_model_result(path)
+    zhfont1 = matplotlib.font_manager.FontProperties(family='Hiragino Sans GB',
+                                                     fname='/System/Library/Fonts/Hiragino Sans GB.ttc')
+    path = '/Users/dongzhejiang/Downloads/all_Log/resnet/cifar1010888.log'
+    model_result_1 = read_model_result(path)
+    path = '/Users/dongzhejiang/Downloads/all_Log/resnet/cifar1010887.log'
+    model_result_2 = read_model_result(path)
+    model_result = (model_result_1 + model_result_2) / 2
+    # 画accuracy图
+    fig, ax = plt.subplots()
+    min_time = model_result['timestamp'].min()
+    x = model_result['timestamp'] - min_time
+    y = model_result['test_acc']
+    plt.plot(x, y, label='GPU = 2')
+    plot_xy(x.max(), y.max(), 0, ax)
+
+    plt.ylabel('accuracy', fontsize=18)
+    plt.xlabel('time(s)', fontsize=18)
+    plt.tick_params(labelsize=18)
+    plt.title('resnet', fontproperties=zhfont1, fontsize=20)
+    plt.xlim([-5, 5000])
+    plt.ylim([0, 1])
+    plt.show()
+
+    #画cpu箱线图
+    fig, ax = plt.subplots()
+    cpu_boxplt=pd.DataFrame()
+    path='/Users/dongzhejiang/Downloads/all_Log/resnet/pid10887.log'
+    cpu_1,gpu_1=read_resource_1(path)
+    path = '/Users/dongzhejiang/Downloads/all_Log/resnet/pid10888.log'
+    cpu_2, gpu_2 = read_resource_1(path)
+    cpu=pd.concat([cpu_1,cpu_2])
+    cpu=cpu.sort_values(by=['timestamp'])
+    cpu['p_cpu_per']=cpu['p_cpu_per']/4000
+    cpu_boxplt['GPU=2']=cpu['p_cpu_per'].copy()
+    cpu_boxplt.boxplot()
+    plt.ylabel('cpu percent', fontsize=18)
+    plt.title('resnet', fontproperties=zhfont1, fontsize=20)
+    plt.tick_params(labelsize=18)
+    plt.show()
+
+    #画gpu箱线图
+    fig, ax = plt.subplots()
+    gpu_boxplot=pd.DataFrame()
+    gpu=pd.concat([gpu_1,gpu_2])
+    gpu=gpu.sort_values(by=['timestamp'])
+    gpu_boxplot['GPU = 2 (1)']=gpu['gpu_1'].copy()
+    gpu_boxplot['GPU = 2 (2)']=gpu['gpu_2'].copy()
+    gpu_boxplot.boxplot()
+    plt.ylabel('gpu percent', fontsize=18)
+    plt.title('resnet', fontproperties=zhfont1, fontsize=20)
+    plt.tick_params(labelsize=18)
+    plt.show()
+
+    #遍历所有数据
+    for root, dirs, files in os.walk('/Users/dongzhejiang/Documents/work/UsersTags/hf_20170601'):
+        i = 0
+        for file in files:
 
     path='/Users/dongzhejiang/Downloads/Log_1/all_process.log'
     cpu_memory,gpu=read_resource(path)
